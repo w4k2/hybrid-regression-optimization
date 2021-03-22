cc3d79f4035916de78503e8d2464a9d0f1745580
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2021-03-22 18:20:39 +0100

    ABCD

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 ### `e00_origins`
 - wykorzystuje `datasets`,
 - produkuje `origins.eps`
+
+### `e01`
+
+- klasyfikator korzysta z oversamplingu (ROS)
+- walidacja krzyżowa
+
+c -> y_cat
+y -> y_reg
+
+a_est -> a_reg
+r_est -> r_reg
+o_est -> o_reg
+
+
+ar-f (AWF)
+aro-f(BWF)
--- e01_main.py
+++ e01_main.py
@@ -1,6 +1,3 @@
-"""
-Weryfikacja jakości modelu regresji w predykcji rozwiazan z optymalizatorow.
-"""
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 from sklearn.neural_network import MLPRegressor, MLPClassifier
@@ -10,6 +7,7 @@ from sklearn.base import clone
 from imblearn.over_sampling import RandomOverSampler, SMOTE
 from imblearn.under_sampling import RandomUnderSampler
 from tqdm import tqdm
+from scipy.stats import ttest_rel
 
 np.set_printoptions(precision=3)
 
@@ -18,7 +16,6 @@ def cevs(y_true, y_pred):
     return np.clip(explained_variance_score(y_true, y_pred), 0, 1)
 
 # Przygotowujemy parametry przetwarzania
-n_splits = 5
 n_best_kaos = 10
 R, O = (0, 1)
 mlp_params = {
@@ -33,139 +30,147 @@ datasets = {
     'US26_G': ('U26G', 83, 'US')
 }
 
-# Estymatory bazowe
+# Base estimators
 base_reg = MLPRegressor(**mlp_params)
 base_clf = MLPClassifier(**mlp_params)
 resampler = RandomOverSampler
 
-# Metryki
+# Experimental protocol
+n_splits = 5
+alpha = .05
 metric_reg = cevs
 metric_clf = balanced_accuracy_score
+skf = StratifiedKFold(n_splits=n_splits, random_state=1410,
+                      shuffle=True)
 
-# Iterujemy zbiory danych
+"""
+Experimental loop
+"""
 for filename in datasets:
     # Load dataset
     dbname, f, kao = datasets[filename] # kao - identyfikator kangurka
     ds = np.load("datasets/%s.npy" % dbname, allow_pickle=True)
-    X = ds[:,:-2]               # dane
-    c = ds[:,-2].astype(int)    # kategorie
-    c[c==2] = 1                 # spłaszczamy kategorie [r=0, o=1]
-    y = ds[:,-1]                # etykiety
+
+    # Establish X, y
+    X = ds[:,:-2]                   # patterns
+    y_cat = ds[:,-2].astype(int)    # flatten categories
+    y_cat[y_cat==2] = 1             # spłaszczamy kategorie [r=0, o=1]
+    y_reg = ds[:,-1]                # regression labels
     X_kao = np.load('kangurki/%s.npy' % kao) # kangurki
 
-    # Przygotowujemy walidację krzyżową
-    skf = StratifiedKFold(n_splits=n_splits, random_state=1410, shuffle=True)
+    print("X", X.shape, "y_cat", y_cat.shape, "y_reg", y_reg.shape,
+          "X_kao", X_kao.shape)
 
-    # Przygotowujemy miejsce na składowanie zespołów
+    # Storage for scores
+    reg_scores = np.zeros((n_splits, 3, 5))     # F x CAT x REG
+    clf_scores = np.zeros(n_splits)             # F
+
+    # Prepare lists for ensembles
     a_ensemble = []
     r_ensemble = []
     o_ensemble = []
     c_ensemble = []
 
-    # Miejsce na składowanie wyników
-    reg_scores = np.zeros((n_splits, 3, 3))     # F x REG x CAT
-    clf_scores = np.zeros(n_splits)             # F
-    wf_reg_scores = np.zeros((n_splits, 2, 3))  # F x REG x CAT
-
-    # Pętla walidacji krzyżowej
-    for fold, (train, test) in tqdm(enumerate(skf.split(X, c)),
+    """
+    CV loop
+    """
+    for fold, (train, test) in tqdm(enumerate(skf.split(X, y_cat)),
                                     total = n_splits):
-        # Inicjalizujemy estymatory
+        # Model initialization
         # a - uczony na wszystkich wzorcach
         # r - uczący na wzorcach losowych
         # o - uczący na wzorcach z optymalizacji
-        a_est, r_est, o_est = [clone(base_reg) for _ in range(3)]
+        a_reg, r_reg, o_reg = [clone(base_reg) for _ in range(3)]
 
-        # Uczymy estymatory
-        a_est.fit(X[train], y[train])
-        r_est.fit(X[train][c[train]==R],
-                  y[train][c[train]==R])
-        o_est.fit(X[train][c[train]==O],
-                  y[train][c[train]==O])
-
-        # Przygotowujemy i uczymy klasyfikator kategorii
-        # [balansując resamplerem]
+        # Building models
+        a_reg.fit(X[train], y_reg[train])
+        r_reg.fit(X[train][y_cat[train]==0], y_reg[train][y_cat[train]==0])
+        o_reg.fit(X[train][y_cat[train]!=0], y_reg[train][y_cat[train]!=0])
         clf = clone(base_clf).fit(*resampler(random_state=1410)
-                                  .fit_resample(X[train], c[train]))
+                                  .fit_resample(X[train], y_cat[train]))
 
-        # Dodajemy je do puli
-        a_ensemble.append(a_est)
-        r_ensemble.append(r_est)
-        o_ensemble.append(o_est)
+        # Adding models to pools
+        a_ensemble.append(a_reg)
+        r_ensemble.append(r_reg)
+        o_ensemble.append(o_reg)
         c_ensemble.append(clf)
 
-        # Zbieramy wszystkie predykcje i probę z klasyfikacji
-        a_y_pred = a_est.predict(X)
-        r_y_pred = r_est.predict(X)
-        o_y_pred = o_est.predict(X)
+        # Gather probas and predictions
+        a_y_pred = a_reg.predict(X)
+        r_y_pred = r_reg.predict(X)
+        o_y_pred = o_reg.predict(X)
         c_y_pred = clf.predict(X)
         c_y_pp = clf.predict_proba(X)
+        arc_y_pred = np.sum([r_y_pred, o_y_pred] * c_y_pp.T, axis=0)
+        aroc_y_pred = (arc_y_pred + a_y_pred) / 2
 
-        """
-        Do a weird fuck
-        """
-        # AWF - ważymy regresory R i O probą z klasyfikatora C
-        awf_y_pred = r_y_pred * c_y_pp[:,0] + o_y_pred * c_y_pp[:,1]
-
-        # BWF - wynik AWF uśredniamy z regresorem A
-        bwf_y_pred = (awf_y_pred + a_y_pred) / 2
-
-        """
-        Wyliczanie metryk
-        """
-        # Wyliczamy wszystkie metryki
-        # REGRESJA
-        # 0 - kategoria ucząca  [a,r,o]
-        # 1 - kategoria testowa [a,r,o]
-        a_a_score = metric_reg(y[test], a_y_pred[test])
-        r_a_score = metric_reg(y[test], r_y_pred[test])
-        o_a_score = metric_reg(y[test], o_y_pred[test])
-        a_r_score = metric_reg(y[test][c[test] == R],
-                               a_y_pred[test][c[test] == R])
-        r_r_score = metric_reg(y[test][c[test] == R],
-                               r_y_pred[test][c[test] == R])
-        o_r_score = metric_reg(y[test][c[test] == R],
-                               o_y_pred[test][c[test] == R])
-        a_o_score = metric_reg(y[test][c[test] == O],
-                               a_y_pred[test][c[test] == O])
-        r_o_score = metric_reg(y[test][c[test] == O],
-                               r_y_pred[test][c[test] == O])
-        o_o_score = metric_reg(y[test][c[test] == O],
-                               o_y_pred[test][c[test] == O])
-
-        # Regresja AWF
-        a_a_wf_score = metric_reg(y[test], awf_y_pred[test])
-        a_r_wf_score = metric_reg(y[test][c[test] == R],
-                                  awf_y_pred[test][c[test] == R])
-        a_o_wf_score = metric_reg(y[test][c[test] == O],
-                                  awf_y_pred[test][c[test] == O])
-
-        b_a_wf_score = metric_reg(y[test], bwf_y_pred[test])
-        b_r_wf_score = metric_reg(y[test][c[test] == R],
-                                  bwf_y_pred[test][c[test] == R])
-        b_o_wf_score = metric_reg(y[test][c[test] == O],
-                                  bwf_y_pred[test][c[test] == O])
-
-        # KLASYFIKACJA
-        clf_score = metric_clf(c[test], c_y_pred[test])
-
-        """
-        Składowanie i prezentacja wyników
-        """
-        # Składowanie wyników
-        reg_scores[fold] = [
-            [a_a_score, a_r_score, a_o_score],
-            [r_a_score, r_r_score, r_o_score],
-            [o_a_score, o_r_score, o_o_score],
+        # Metric calculations
+        ## Prepare
+        a_ground = y_cat[test] >= 0
+        r_ground = y_cat[test] == 0
+        o_ground = y_cat[test] != 0
+        grounds = [a_ground, r_ground, o_ground]
+        regs_preds = [
+            a_y_pred, r_y_pred, o_y_pred,
+            arc_y_pred, aroc_y_pred
         ]
-        wf_reg_scores[fold] = [
-            [a_a_wf_score, a_r_wf_score, a_o_wf_score],
-            [b_a_wf_score, b_r_wf_score, b_o_wf_score],
-        ]
+        freg_scores = np.zeros((5,3)) # Approach x Category
+
+        ## Calculate
+        for pred_idx, y_pred in enumerate(regs_preds):
+            for ground_idx, ground in enumerate(grounds):
+                score = metric_reg(y_reg[test][ground],
+                                   y_pred[test][ground])
+                freg_scores[pred_idx, ground_idx] = score
+        clf_score = metric_clf(y_cat[test], c_y_pred[test])
+
+        ## Store
+        reg_scores[fold] = freg_scores.T
         clf_scores[fold] = clf_score
 
     print("\nDATASET", filename, dbname, f)
-    print("\nBase regressors # [A R O] x [A R O]")
+
+    # Analysis
+    mean_reg_scores = np.mean(reg_scores, axis=0)
+    std_reg_scores = np.std(reg_scores, axis=0)
+
+    # Statistical analysis
+    contexts = ["\\textsc{all}", "\\textsc{ran}", "\\textsc{opt}"]
+    for context in range(3):
+        relevance = np.array([[ttest_rel(reg_scores[:, context, a_idx],
+                                       reg_scores[:, context, b_idx]).pvalue
+                             for a_idx in range(5)]
+                            for b_idx in range(5)]) < alpha
+        superiority = np.array([[ttest_rel(reg_scores[:, context, a_idx],
+                                          reg_scores[:, context, b_idx]).statistic
+                                for a_idx in range(5)]
+                               for b_idx in range(5)]) < 0
+        relevant_superiority = relevance * superiority
+
+        conclusion = [list(np.where(_)[0] + 1)
+                      for _ in relevant_superiority]
+
+        # LaTeX table
+        line_a = mean_reg_scores[context]
+        line_b = std_reg_scores[context]
+        line_c = conclusion
+
+        line_a = "%s & " % contexts[context] + " & ".join(["%.3f" % v
+                                                           for v in line_a])
+        line_b = " & " + " & ".join(["\\scriptsize (%.2f)" % v
+                                     for v in line_b])
+        line_c = " & " + " & ".join([",".join(["\\scriptsize\\oldstylenums{%i}" % vv
+                                               for vv in v]) if len(v) > 0 else "---"
+                                     for v in line_c])
+
+
+        print(line_a, "\\\\")
+        print(line_b, "\\\\")
+        print(line_c, "\\\\")
+
+    # exit()
+
+    """
     print(np.mean(reg_scores, axis=0))
     print("\nCategorical classifier")
     print("%.3f (%.2f)" % (np.mean(clf_scores), np.std(clf_scores)))
@@ -205,9 +210,7 @@ for filename in datasets:
     print("BWF-R %.3f" % metric_reg(y[c==R], bwf_y_pred[c==R]))
     print("BWF-O %.3f" % metric_reg(y[c==O], bwf_y_pred[c==O]))
 
-    """
-    Kangurze rezultaty
-    """
+    # Kangurze rezultaty
     est_a_preds = np.mean(
         np.array([est.predict(X_kao) for est in a_ensemble]), axis=0)
     est_r_preds = np.mean(
@@ -289,4 +292,5 @@ for filename in datasets:
 
     np.save("results/%s-o-X" % filename, X_aro_candidates[o_idx])
     np.save("results/%s-o-y" % filename, o_candidates[o_idx])
-    # exit()
+    """
+    #exit()
